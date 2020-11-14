ee49f5024199bb006d72bd8f7e4e0578f65b271d
zhangsan <2655904102@qq.com>
2020-11-14 14:27:33 +0800

    第一次提交项目

---
+++ src/com/bjpowernode/github/bean/User.java
@@ -0,0 +1,18 @@
+package com.bjpowernode.github.bean;
+
+/**
+ * @ProjectName: gihubDemo
+ * @Package: com.bjpowernode.github.bean
+ * @Description: java类作用描述
+ * @Author: Andy
+ * @CreateDate: 2020/11/14 14:22
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2020
+ */
+public class User {
+
+    private String name;
+    private int age;
+
+}
